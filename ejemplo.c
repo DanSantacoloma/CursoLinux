2354b46acec00f39fb03c6c9b7bb261325ecf686
DanSantacoloma <dansantacoloma16@gmail.com>
2020-05-23 17:19:22 -0500

    Primer commit

---
+++ ejemplo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+printf("************************************************\n");
+printf("Practica: configurar y usar GitHub en Linux RHE*\n");
+printf("************************************************\n");
+return 0;
+}
